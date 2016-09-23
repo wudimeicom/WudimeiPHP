05033b27f0142945342a794c853491b78701374d
Yang Qing-rong <admin@wudimei.com>
2016-09-23 08:58:00 +0800

    config

--- Config.php
+++ Config.php
@@ -6,40 +6,42 @@
  * @license The MIT license(MIT)
  */
 namespace Wudimei;
-
-class Config{
-	public $dir;
-	public $data;
-	public function setDir( $dir ){
-		$this->dir = $dir;
-	}
 	
-	public function get( $keys ){
+	class Config{
+		public $dir;
+		public $data;
+		public function setDir( $dir ){
+			$this->dir = $dir;
+		}
 		
-		$arr = explode(".", $keys);
-		$sectionName = $arr[0];
-		$this->load($sectionName);
-		//print_r( $this->data);
-		return ArrayHelper::fetch( $this->data, $keys);
+		public function get( $keys ){
+			
+			$arr = explode(".", $keys);
+			$sectionName = $arr[0];
+			$this->load($sectionName);
+			//print_r( $this->data);
+			return ArrayHelper::fetch( $this->data, $keys);
+			
+		}
 		
-	}
-	
-	public function load( $sectionName ){
-		if( !isset($this->data[ $sectionName] )){
-			$file = $this->dir . "/" . $sectionName . ".php";
-			$this->data[ $sectionName] = [];
-			if( file_exists( $file)){
-				$this->data[ $sectionName] = include $file;
+		public function load( $sectionName ){
+			if( !isset($this->data[ $sectionName] )){
+				$file = $this->dir . "/" . $sectionName . ".php";
+				$this->data[ $sectionName] = [];
+				if( file_exists( $file)){
+					$this->data[ $sectionName] = include $file;
+				}
+					
 			}
-				
+		}
+		public function set( $keys , $value ){
+			$arr = explode(".", $keys);
+			$sectionName = $arr[0];
+			$this->load($sectionName);
+			
+			 ArrayHelper::set( $this->data, $keys, $value);
+			 //print_r( $this->data );
 		}
 	}
-	public function set( $keys , $value ){
-		$arr = explode(".", $keys);
-		$sectionName = $arr[0];
-		$this->load($sectionName);
-		
-		 ArrayHelper::set( $this->data, $keys, $value);
-		 //print_r( $this->data );
-	}
-}
+
+
